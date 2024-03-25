b51cf4c61a9c89ea96bbccace4b28d566c2fe00b
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2024-03-25 14:12:43 -0600

    Estacionamiento UAEM

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:estacionamiento_uaem/screens/disponibilidad_screen.dart';
+import 'package:estacionamiento_uaem/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: DisponibilidadScreen(),
+      home: LoginScreen(),
     );
   }
 }
